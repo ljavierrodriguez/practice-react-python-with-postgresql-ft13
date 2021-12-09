dda603747b4b616ac95db52046ab6deed39e1d0c
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2021-12-09 19:03:16 +0000

    List, create update  and delete contacts

---
+++ api/src/app.py
@@ -0,0 +1,98 @@
+from flask import Flask, request, jsonify, render_template
+from flask_migrate import Migrate
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager, get_jwt_identity, jwt_required, create_access_token
+from models import db, User, Contact
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.config['DEBUG'] = True 
+app.config['ENV'] = 'development' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'dialect+driver://user:pass@host:port/database' 
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://gitpod@localhost:5432/prueba' 
+app.config['JWT_SECRET_KEY'] = 'my-secret-key'
+
+db.init_app(app)
+Migrate(app, db)
+CORS(app)
+jwt = JWTManager(app)
+
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+@app.route('/api/users', methods=['GET'])
+@app.route('/api/users/<int:id>', methods=['GET'])
+def users(id = None):
+    if request.method == 'GET':
+        if id is not None:
+            user = Contact.query.get(id)
+            if not user: return jsonify({ "msg": "User not found!"}), 404
+            return jsonify(user.serialize()), 200
+        else:
+            users = User.query.all()
+            users = list(map(lambda user: user.serialize(), users))
+            return jsonify(users), 200
+
+@app.route('/api/contacts', methods=['GET', 'POST'])
+@app.route('/api/contacts/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def contacts(id = None):
+    if request.method == 'GET':
+        if id is not None:
+            contact = Contact.query.get(id)
+            if not contact: return jsonify({ "msg": "Contact not found!"}), 404
+            return jsonify(contact.serialize()), 200
+        else:
+            contacts = Contact.query.order_by("id").all()
+            contacts = list(map(lambda contact: contact.serialize(), contacts))
+            return jsonify(contacts), 200
+
+    if request.method == 'POST':
+        name = request.json.get("name")
+        phone = request.json.get("phone")
+        email = request.json.get("email", "")
+        user_id = request.json.get("user_id")
+
+        if not name: return jsonify({ "msg": "Name is required!"}), 400
+        if not phone: return jsonify({ "msg": "Phone is required!"}), 400
+        if not user_id: return jsonify({ "msg": "User is required!"}), 400
+
+        contact = Contact()
+        contact.name = name
+        contact.phone = phone
+        contact.email = email
+        contact.user_id = user_id
+        contact.save()
+
+        return jsonify(contact.serialize()), 201
+
+    if request.method == 'PUT':
+        name = request.json.get("name")
+        phone = request.json.get("phone")
+        email = request.json.get("email", "")
+
+        if not name: return jsonify({ "msg": "Name is required!"}), 400
+        if not phone: return jsonify({ "msg": "Phone is required!"}), 400
+
+        contact = Contact.query.get(id)
+        if not contact: return jsonify({ "msg": "Contact not found!"}), 404
+
+        contact.name = name
+        contact.phone = phone
+        contact.email = email
+        contact.update()
+
+        return jsonify(contact.serialize()), 200
+
+    if request.method == 'DELETE':
+        contact = Contact.query.get(id)
+        if not contact: return jsonify({ "msg": "Contact not found!"}), 404
+
+        contact.delete()
+
+        return jsonify({ "success": "User was deleted!"}), 200
+
+if __name__ == '__main__':
+    app.run()
---
+++ api/src/models.py
@@ -0,0 +1,60 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    contacts = db.relationship('Contact', backref='user') 
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "contacts": self.get_contacts()
+        }
+
+    def get_contacts(self):
+        return list(map(lambda contact: contact.serialize(), self.contacts))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Contact(db.Model):
+    __tablename__ = 'contacts'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    phone = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "phone": self.phone,
+            "email": self.email
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
